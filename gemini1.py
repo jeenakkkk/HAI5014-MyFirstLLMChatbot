61967b0bbacba95009a855e3ea2eae56eeaa1734
Sehyun Kim <kimjeena94@skku.edu>
2025-04-10 07:18:45 +0000

    add factors

--- gemini1.py
+++ gemini1.py
@@ -2,11 +2,10 @@ import os
 from openai import OpenAI
 
 token = os.environ["GOOGLE_API_KEY"]
-endpoint = "https://models.inference.ai.azure.com"
-model_name = "gpt-4o"
+endpoint = "https://generativelanguage.googleapis.com/v1beta/openai/"
+model_name = "gemini-2.0-flash"
 
 client = OpenAI(
-    base_url=endpoint,
     api_key=token,
 )
 
---
+++ geminimodels.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    api_key=os.environ["GOOGLE_API_KEY"],
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    n=1,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+print(response.choices[0].message)
